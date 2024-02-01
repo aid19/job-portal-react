9cd1781d11fe99424462ca1bd7e56c258634103e
Aida Ismayilova <aIsmayilova@socartrading.com>
2024-02-01 21:05:59 +0100

    Search Bar

--- src/Components/SearchDiv/Search.jsx
+++ src/Components/SearchDiv/Search.jsx
@@ -1,6 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import { AiOutlineSearch } from 'react-icons/ai';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
+import { BsHouseDoor } from 'react-icons/bs';
+import { CiLocationOn } from 'react-icons/ci';
 
 const Search = () => {
   return (
@@ -9,10 +12,27 @@ const Search = () => {
             <div className='firstDiv flex justify-between items-center rounde-[8px] gap-[10px] bg-white p-5
             shadow-lg shadow-greyIsh-700'>
                 <div className='flex gap-2 items-center'>
-                    <AiOutlineSearch />
+                    <AiOutlineSearch className='text-[25px] icon'/>
+                    <input type='text' className='bg-transparent text-blue-500 focus:otline-none w-[100%]' placeholder='Search Job Here..' />
+                  <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a6] hover:text-textColor icon'/>
                 </div>
+
+                <div className='flex gap-2 items-center'>
+                    <BsHouseDoor className='text-[25px] icon'/>
+                    <input type='text' className='bg-transparent text-blue-500 focus:otline-none w-[100%]' placeholder='Search by company..' />
+                  <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a6] hover:text-textColor icon'/>
+                </div>
+
+                <div className='flex gap-2 items-center'>
+                    <CiLocationOn className='text-[25px] icon'/>
+                    <input type='text' className='bg-transparent text-blue-500 focus:otline-none w-[100%]' placeholder='Search by location..' />
+                  <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a6] hover:text-textColor icon'/>
+                </div>
+
+                <button className='bg-blueColor h-full p-5 px-10 rounded-[10px] text-white cursor-pointer hover:bg-blue-300'>Search</button>
             </div>
         </from>
+        
     </div>
   )
 }
