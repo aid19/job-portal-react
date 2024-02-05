88bc1831da65c7ef80fa6b0fdf9bec9ab9f29e76
Aida Ismayilova <aIsmayilova@socartrading.com>
2024-02-05 20:08:10 +0100

    some changes

--- src/Components/JobDiv/Jobs.jsx
+++ src/Components/JobDiv/Jobs.jsx
@@ -1,9 +1,28 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 
+import {BiTimeFive} from 'react-icons/bi'
+
 const Jobs = () => {
   return (
-    <div>Jobs</div>
+    <div className='jobContainer flex gap-10 justify-center flex-wrap items-center py-10'>
+      <div className='group group/item singleJob w-[250px] p-[20px] bg-white rounded [10p] hover:bg-blueColor shadow-lg 
+      shadow-greyIsh-400/700 hover:shadow-lg'>
+        <span className='flex justify-between items-center gap-4'>
+          <h1 className='text-[16px] font-semibold text-textColor group-hover:text-white'>Web Developer</h1>
+          <span className='flex items-center text-[#ccc]'>
+            <BiTimeFive />Now
+          </span>
+          <h6 className='text-[#ccc]'></h6>
+          <p className='text-[13px] text-[#959595] pt-[20px] border-t-[2px] mt-[20px] group-hover:text-white'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex, id.
+          </p>
+          <div className='company flex items-center gap-2'>
+            <img src='' alt=''/>
+          </div>
+        </span>
+      </div>
+    </div>
   )
 }
 
--- src/Components/SearchDiv/Search.jsx
+++ src/Components/SearchDiv/Search.jsx
@@ -7,7 +7,7 @@ import { CiLocationOn } from 'react-icons/ci';
 
 const Search = () => {
   return (
-    <div className='searchDiv grid gb-10 bg-greyIsh reounded-[10px] p-[3rem]'>
+    <div className='searchDiv grid gap-10 bg-greyIsh reounded-[10px] p-[3rem]'>
         <from action="">
             <div className='firstDiv flex justify-between items-center rounde-[8px] gap-[10px] bg-white p-5
             shadow-lg shadow-greyIsh-700'>
@@ -32,7 +32,37 @@ const Search = () => {
                 <button className='bg-blueColor h-full p-5 px-10 rounded-[10px] text-white cursor-pointer hover:bg-blue-300'>Search</button>
             </div>
         </from>
-        
+
+        <div className="secDiv flex items-center gap-10 justify-center">
+          <div className='singleSearch flex items-center gap-2'>
+            <label htmlFor="relevance" className='text-[808080] font-semibold'>Sort by:</label>
+            <select name="" id="relevance" className='bg-white rounded-[3px] px-4 py-1'>
+              <option value="">Relevance</option>
+              <option value="">Inclusive</option>
+              <option value="">Start with</option>
+              <option value="">Contains</option>
+            </select>
+          </div>
+          <div className='singleSearch flex items-center gap-2'>
+            <label htmlFor="type" className='text-[808080] font-semibold'>Type:</label>
+            <select name="" id="tyoe" className='bg-white rounded-[3px] px-4 py-1'>
+              <option value="">Full-time</option>
+              <option value="">Remote</option>
+              <option value="">Contract</option>
+              <option value="">Part-time</option>
+            </select>
+          </div>
+          <div className='singleSearch flex items-center gap-2'>
+            <label htmlFor="level" className='text-[808080] font-semibold'>Level:</label>
+            <select name="" id="level" className='bg-white rounded-[3px] px-4 py-1'>
+              <option value="">Senior</option>
+              <option value="">Beginner</option>
+              <option value="">Intermediate</option>
+              <option value="">Advocate</option>
+            </select>
+          </div>
+          <span className='text-[#a1a1a1] cursor-pointer'>Clear All</span>
+        </div>
     </div>
   )
 }
